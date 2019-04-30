e386c76691041974fe9c39270fde17198ec4cb02
wangzhifei <17197880306@163.com>
2019-04-30 16:15:06 +0800

    1、本地安装erlang和rabbitmq的配置，启动rabbitmq的服务 2、使用SpringCloudBus通过RabbitMQ提供消息总线的服务 3、Git更新文件，通过访问http://localhost:9031/actuator/bus-refresh来更新客户端的配置

--- configclient/src/main/java/com/mirana/configclient/MysqlController.java
+++ configclient/src/main/java/com/mirana/configclient/MysqlController.java
@@ -31,6 +31,29 @@ public class MysqlController {
      * 访问 http://localhost:9031/getMysqlConfig，返回数据如下：
      * {"password":"1234","port":"3306","host":"loaclhost","username":"root"}
      *
+     * 20190430
+     * 1、更新配置文件password 1234->5678
+     * 2、POST 访问地址 http://localhost:9031/actuator/bus-refresh
+     *      这里使用MINGW64提交Post请求
+     *      Assassin@mirana MINGW64 ~
+     *      $ curl -v -X POST "http://localhost:9031/actuator/bus-refresh"
+     *        % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+     *                                       Dload  Upload   Total   Spent    Left  Speed
+     *        0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0*   Trying ::1...
+     *      * TCP_NODELAY set
+     *      * Connected to localhost (::1) port 9031 (#0)
+     *      > POST /actuator/bus-refresh HTTP/1.1
+     *      > Host: localhost:9031
+     *      > User-Agent: curl/7.60.0
+     *      > Accept: ...
+     *      >0     0    0     0    0     0      0      0 --:--:--  0:00:14 --:--:--     0< HTTP/1.1 204
+     *      < Date: Tue, 30 Apr 2019 08:07:20 GMT
+     *      <
+     *        0     0    0     0    0     0      0      0 --:--:--  0:00:14 --:--:--     0
+     *      * Connection #0 to host localhost left intact
+     * 3、重新访问 http://localhost:9031/getMysqlConfig，返回数据如下：
+     * {"password":"5678","port":"3306","host":"loaclhost","username":"root"}
+     *
      * @return mysqlConfigJson
      */
     @GetMapping("/getMysqlConfig")
